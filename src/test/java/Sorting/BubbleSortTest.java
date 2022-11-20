0c88856adbd2c4f1575a1e25ac5dc5916fa7efbb
Rushikesh <kalerushikesh32@gmail.com>
2022-11-20 23:58:59 +0530

    Added sorting algos

--- src/test/java/Sorting/BubbleSortTest.java
+++ src/test/java/Sorting/BubbleSortTest.java
@@ -13,6 +13,7 @@ public class BubbleSortTest {
 
     @Test
     public void test1() {
+
         BubbleSort bs = new BubbleSort();
         ArrayList<Integer> exp2 = new ArrayList<Integer>(Arrays.asList(1,2,3,6));
         ArrayList<Integer> act2 = new ArrayList<Integer>(Arrays.asList(1,2,6,3));
